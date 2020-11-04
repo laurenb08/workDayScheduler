646fcce9e870fb1ff91d32a8e8bd4d2fba00fd2b
Lauren Plenger <laurenb08@gmail.com>
2020-11-03 23:13:16 -0800

    added styling to textarea and started working on local storage function

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,13 @@
+var time = moment();
+
 setInterval (function() {
-    var time = moment();
     $("#currentDay").text(time.format("dddd MMMM Do"));
 }, 1000);
+
+
+function saveAppointment() {
+    var formInput = document.getElementById("apptInput").value ;
+    localStorage.setItem("apptInput", formInput);
+    alert("Appointment saved!") ;
+    console.log(formInput);
+}
