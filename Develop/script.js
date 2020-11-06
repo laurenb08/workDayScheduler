33de2fd775e4c91f6297cdfe08234453a5862a37
Lauren Plenger <laurenb08@gmail.com>
2020-11-05 16:40:42 -0800

    added the time comparison and conditions to modify the background based on the time

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,8 @@
 var time = moment();
+var compareTime = moment().format("hA");
+var apptTime = document.getElementById("hour").textContent;
+console.log (compareTime);
+console.log (apptTime);
 
 setInterval (function() {
     $("#currentDay").text(time.format("dddd MMMM Do"));
@@ -10,4 +14,16 @@ function saveAppointment() {
     localStorage.setItem("apptInput", formInput);
     alert("Appointment saved!") ;
     console.log(formInput);
+}
+
+// if value in the hour class is > = < current hour
+if (compareTime == apptTime) {
+    var textClass = $("apptInput");
+    document.getElementById("apptInput").classList.add("present");
+} else if (compareTime > apptTime) {
+    var textClass = $("apptInput");
+    document.getElementById("apptInput").classList.add("past");
+} else if (compareTime < apptTime) {
+    var textClass = $("apptInput");
+    document.getElementById("apptInput").classList.add("future");
 }
