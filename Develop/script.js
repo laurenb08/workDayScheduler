04018c5db363d074c2230752aa82a030c9ea054c
Lauren Plenger <laurenb08@gmail.com>
2020-11-07 16:59:24 -0800

    fixing local storage issues

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
 var time = moment();
-var compareTime = moment().format("hA");
-var apptTime = $("#hour").val();
-console.log(compareTime);
-console.log(apptTime);
+// var compareTime = moment().format("hA");
+// var apptTime = $("#hour").val();
+// console.log(compareTime);
+// console.log(apptTime);
 
 setInterval(function () {
     $("#currentDay").text(time.format("dddd MMMM Do"));
@@ -10,11 +10,11 @@ setInterval(function () {
 
 
 function saveAppointment() {
-    var formInput = document.getElementById("apptInput").value;
-    localStorage.setItem("apptInput", formInput);
+    var textarea9 = document.getElementById("textarea9").value;
+    localStorage.setItem("textarea9", textarea9);
     alert("Appointment saved!");
-    console.log(formInput);
-} loadAppointment();
+    console.log(localStorage);
+}
 
 // if value in the hour class is > = < current hour
 // if (compareTime.textContent == apptTime.textContent) {
@@ -30,9 +30,27 @@ function saveAppointment() {
 
 // For 9AM
 
+loadAppointment();
+
 function loadAppointment() {
     var nineBtn = localStorage.getItem("nineBtn");
-    document.getElementById("nineBtn").value = nineBtn;
+    document.getElementById("textarea9").value = nineBtn;
+    // var tenBtn = localStorage.getItem("tenBtn");
+    // document.getElementById("textarea10").value = tenBtn;
+    // var elevenBtn = localStorage.getItem("elevenBtn");
+    // document.getElementById("textarea11").value = elevenBtn;
+    // var twelveBtn = localStorage.getItem("twelveBtn");
+    // document.getElementById("textarea12").value = twelveBtn;
+    // var thirteenBtn = localStorage.getItem("thirteenBtn");
+    // document.getElementById("textarea13").value = thirteenBtn;
+    // var fourteenBtn = localStorage.getItem("fourteenBtn");
+    // document.getElementById("textarea14").value = fourteenBtn;
+    // var fifteenBtn = localStorage.getItem("fifteenBtn");
+    // document.getElementById("textarea15").value = fifteenBtn;
+    // var sixteenBtn = localStorage.getItem("sixteenBtn");
+    // document.getElementById("textarea16").value = sixteenBtn;
+    // var seventeenBtn = localStorage.getItem("seventeenBtn");
+    // document.getElementById("textarea17").value = seventeenBtn;
 }
 
 var present = new Date().getHours();
