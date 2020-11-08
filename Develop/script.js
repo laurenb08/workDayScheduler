b1efb347301769a6a162f9bbff61f154f607c6b2
Lauren Plenger <laurenb08@gmail.com>
2020-11-07 16:58:51 -0800

    changed html table elements to simplify theme

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
 var time = moment();
 var compareTime = moment().format("hA");
-var apptTime = $("#hour").text();
-console.log (compareTime);
-console.log (apptTime);
+var apptTime = $("#hour").val();
+console.log(compareTime);
+console.log(apptTime);
 
-setInterval (function() {
+setInterval(function () {
     $("#currentDay").text(time.format("dddd MMMM Do"));
 }, 1000);
 
@@ -12,18 +12,39 @@ setInterval (function() {
 function saveAppointment() {
     var formInput = document.getElementById("apptInput").value;
     localStorage.setItem("apptInput", formInput);
-    alert("Appointment saved!") ;
+    alert("Appointment saved!");
     console.log(formInput);
-}
+} loadAppointment();
 
 // if value in the hour class is > = < current hour
-if (compareTime.textContent == apptTime.textContent) {
-    // var textClass = $("apptInput").textContent;
-    $("#apptInput").addClass("present");
-} else if (compareTime.textContent >= apptTime.textContent) {
-    // var textClass = $("apptInput").textContent;
-    $("#apptInput").addClass("past");
-} else if (compareTime.textContent <= apptTime.textContent) {
-    // var textClass = $("apptInput").textContent;
-    $("#apptInput").addClass("future");
+// if (compareTime.textContent == apptTime.textContent) {
+// var textClass = $("apptInput").textContent;
+// $("#apptInput").addClass("present");
+// } else if (compareTime.textContent >= apptTime.textContent) {
+// var textClass = $("apptInput").textContent;
+//     $("#apptInput").addClass("past");
+// } else if (compareTime.textContent <= apptTime.textContent) {
+// var textClass = $("apptInput").textContent;
+//     $("#apptInput").addClass("future");
+// }
+
+// For 9AM
+
+function loadAppointment() {
+    var nineBtn = localStorage.getItem("nineBtn");
+    document.getElementById("nineBtn").value = nineBtn;
 }
+
+var present = new Date().getHours();
+
+for (i = 9; i < 18; i++) {
+    if (present > i) {
+        $("#textarea" + i).addClass("past");
+    }
+    else if (present == i) {
+        $("#textarea" + i).addClass("present");
+    }
+    else if (present < i) {
+        $("#textarea" + i).addClass("future");
+    }
+};
